be8532ce00812f084bd1edd1245a9d969bb003df
Andrea Amorosi <aamorosi@amazon.es>
2022-11-02 12:33:51 +0100

    feat: added middleware

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log("Middleware", request);
+
+  return NextResponse.next({
+    headers: {
+      something: "foo",
+    },
+  });
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/pokemon/:path*",
+};
